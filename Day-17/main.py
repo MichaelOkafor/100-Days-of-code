677e051ec2f5714d579cffe3283ec926c6fa90c6
MichaelOkafor <Omichael091@outlook.com>
2022-07-17 04:40:24 +0100

    update to Day 17

--- Day-17/main.py
+++ Day-17/main.py
@@ -14,3 +14,5 @@ quiz = QuizBrain(question_bank)
 
 while quiz.still_has_questions():
     quiz.next_question()
+print("You have completed the quiz.")
+print(f"Your final score is: {quiz.score}/{quiz.question_number}")
